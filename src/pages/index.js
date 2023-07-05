b8971d0ccb3646ea388c0711389d201655179c1a
Davichobits <davidruizdeveloper@gmail.com>
2023-07-05 16:39:10 -0500

    Markdown en italic

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
 
         <p className='paragraph'>Estos archivos <i>Markdown</i> normalmente contienen <i>links</i> (vínculos/ligas) que muchas veces están rotos o ya no son válidos y eso perjudica mucho el valor de la información que se quiere compartir.</p>
 
-        <p className='paragraph'>Dentro de una comunidad de código abierto, nos han propuesto crear una herramienta usando <a href="https://nodejs.org/">Node.js</a>, que lea y analice archivos en formato `Markdown`, para verificar los links que contengan y reportar algunas estadísticas.</p>
+        <p className='paragraph'>Dentro de una comunidad de código abierto, nos han propuesto crear una herramienta usando <a href="https://nodejs.org/">Node.js</a>, que lea y analice archivos en formato <i>Markdown</i>`, para verificar los links que contengan y reportar algunas estadísticas.</p>
 
         <img src="https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg" alt="mdlinks image" />
         
