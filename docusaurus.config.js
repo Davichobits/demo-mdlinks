380988c8d6e904a46543963e4dff54b94231bae4
Davichobits <davidruizdeveloper@gmail.com>
2023-07-20 14:30:46 -0500

    projectName demo-mdlinks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'davichobits', // Usually your GitHub org/user name.
-  projectName: 'mdlinks', // Usually your repo name.
+  projectName: 'demo-mdlinks', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
